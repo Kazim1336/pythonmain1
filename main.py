fbf890b206e2acf17579a296ee14ae9bb5817728
Максим <zaitsev1336@gmail.com>
2022-09-18 16:57:11 +0400

    First

---
+++ main.py
@@ -0,0 +1,18 @@
+num1 = int(input("введите первое число: "))
+
+num2 = int(input("введите второе число: "))
+
+num1 += 5
+
+print("result: ", num1 + num2)
+print("result: ", num1 - num2)
+print("result: ", num1 * num2)
+print("result: ", num1 ** num2)
+print("result: ", num1 / num2)
+print("result: ", num1 // num2)
+
+word = "HI"
+print(word * 2)
+
+word = "Hello"
+print(word * 2)
